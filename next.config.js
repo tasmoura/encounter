bc65c0b41896630dae5c56e79f02d067fa7cb7a4
tasmoura <57878857+tasmoura@users.noreply.github.com>
2024-11-02 12:10:25 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-  output: "export",
+  output: 'export', // Para exportar como estático
+  distDir: 'out',   // Define o diretório de saída como 'out'
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
